c0d067ca1e2e82402b2ebff5f67f67901d0dc428
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-10 18:38:58 +0200

    starting with ch. 3 example

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -1,97 +1,91 @@
-var express = require("express");
-var request = require("sync-request");
-var url = require("url");
-var qs = require("qs");
-var querystring = require('querystring');
-var cons = require('consolidate');
-var randomstring = require("randomstring");
-var __ = require('underscore');
-__.string = require('underscore.string');
+var express = require("express")
+var request = require("sync-request")
+var url = require("url")
+var qs = require("qs")
+var querystring = require("querystring")
+var cons = require("consolidate")
+var randomstring = require("randomstring")
+var __ = require("underscore")
+__.string = require("underscore.string")
 
-var app = express();
+var app = express()
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/client');
+app.engine("html", cons.underscore)
+app.set("view engine", "html")
+app.set("views", "files/client")
 
 // authorization server information
 var authServer = {
-	authorizationEndpoint: 'http://localhost:9001/authorize',
-	tokenEndpoint: 'http://localhost:9001/token'
-};
+  authorizationEndpoint: "http://localhost:9001/authorize",
+  tokenEndpoint: "http://localhost:9001/token",
+}
 
 // client information
-
 
 /*
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
-	"redirect_uris": ["http://localhost:9000/callback"]
-};
+  client_id: "oauth-client-1",
+  client_secret: "oauth-client-secret-1",
+  redirect_uris: ["http://localhost:9000/callback"],
+}
 
-var protectedResource = 'http://localhost:9002/resource';
+var protectedResource = "http://localhost:9002/resource"
 
-var state = null;
+var state = null
 
-var access_token = null;
-var scope = null;
+var access_token = null
+var scope = null
 
-app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, scope: scope});
-});
+app.get("/", function (req, res) {
+  res.render("index", { access_token: access_token, scope: scope })
+})
 
-app.get('/authorize', function(req, res){
+app.get("/authorize", function (req, res) {
+  /*
+   * Send the user to the authorization server
+   */
+})
 
-	/*
-	 * Send the user to the authorization server
-	 */
-	
-});
+app.get("/callback", function (req, res) {
+  /*
+   * Parse the response from the authorization server and get a token
+   */
+})
 
-app.get('/callback', function(req, res){
+app.get("/fetch_resource", function (req, res) {
+  /*
+   * Use the access token to call the resource server
+   */
+})
 
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
-	
-});
+var buildUrl = function (base, options, hash) {
+  var newUrl = url.parse(base, true)
+  delete newUrl.search
+  if (!newUrl.query) {
+    newUrl.query = {}
+  }
+  __.each(options, function (value, key, list) {
+    newUrl.query[key] = value
+  })
+  if (hash) {
+    newUrl.hash = hash
+  }
 
-app.get('/fetch_resource', function(req, res) {
+  return url.format(newUrl)
+}
 
-	/*
-	 * Use the access token to call the resource server
-	 */
-	
-});
+var encodeClientCredentials = function (clientId, clientSecret) {
+  return Buffer.from(
+    querystring.escape(clientId) + ":" + querystring.escape(clientSecret)
+  ).toString("base64")
+}
 
-var buildUrl = function(base, options, hash) {
-	var newUrl = url.parse(base, true);
-	delete newUrl.search;
-	if (!newUrl.query) {
-		newUrl.query = {};
-	}
-	__.each(options, function(value, key, list) {
-		newUrl.query[key] = value;
-	});
-	if (hash) {
-		newUrl.hash = hash;
-	}
-	
-	return url.format(newUrl);
-};
+app.use("/", express.static("files/client"))
 
-var encodeClientCredentials = function(clientId, clientSecret) {
-	return Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
-};
-
-app.use('/', express.static('files/client'));
-
-var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
-});
- 
+var server = app.listen(9000, "localhost", function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("OAuth Client is listening at http://%s:%s", host, port)
+})
