aceb1d19bc9d42ebcb73e704533af00b7563903e
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-16 17:26:26 +0200

    p.50

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -43,6 +43,8 @@ app.get("/", function (req, res) {
 })
 
 app.get("/authorize", function (req, res) {
+  access_token = null
+
   /*
    * Send the user to the authorization server
    */
@@ -51,13 +53,51 @@ app.get("/authorize", function (req, res) {
     client_id: client.client_id,
     redirect_uri: client.redirect_uris[0],
   })
+  console.log("redirect", authorizeUrl)
   res.redirect(authorizeUrl)
 })
 
 app.get("/callback", function (req, res) {
+  if (req.query.error) {
+    res.render("error", { error: req.query.error })
+    return
+  }
+
   /*
    * Parse the response from the authorization server and get a token
    */
+  var code = req.query.code
+  var form_data = qs.stringify({
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: client.redirect_uris[0],
+  })
+
+  var headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization:
+      "Basic " +
+      encodeClientCredentials(client.client_id, client.client_secret),
+  }
+
+  var tokRes = request("POST", authServer.tokenEndpoint, {
+    body: form_data,
+    headers: headers,
+  })
+
+  console.log("Requesting access token for code %s", code)
+
+  if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+    var body = JSON.parse(tokRes.getBody())
+    access_token = body.access_token
+    console.log("Got access token: %s", access_token)
+    res.render("index", { access_token: body.access_token })
+  } else {
+    res.render("error", {
+      error:
+        "Unable to fetch access token, server response: " + tokRes.statusCode,
+    })
+  }
 })
 
 app.get("/fetch_resource", function (req, res) {
