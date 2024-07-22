1f9c0a12879474255ac067b4a6b508c1666939e2
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-22 17:11:31 +0200

    token check implemented (no scopes yet)

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -7,6 +7,7 @@ var cors = require("cors")
 
 var app = express()
 
+// app.all("*", getAccessToken)
 app.use(bodyParser.urlencoded({ extended: true })) // support form-encoded bodies (for bearer tokens)
 
 app.engine("html", cons.underscore)
@@ -26,9 +27,36 @@ var getAccessToken = function (req, res, next) {
   /*
    * Scan for an access token on the incoming request.
    */
-  var token = null
+  var inToken = null
   var auth = req.headers["authorization"]
   if (auth && auth.toLowerCase().indexOf("bearer") == 0) {
+    inToken = auth.slice("bearer ".length)
+  } else if (req.body && req.body.access_token) {
+    inToken = req.body.access_token
+  } else if (req.query && req.query.access_token) {
+    inToken = req.query.access_token
+  }
+  console.log("Incoming token, %s", inToken)
+  nosql.one().make(function (builder) {
+    builder.where("access_token", inToken)
+    builder.callback(function (err, token) {
+      if (token) {
+        console.log("We found a matching token: %s", inToken)
+      } else {
+        console.log("No matching token was found.")
+      }
+      req.access_token = token
+      next()
+      return
+    })
+  })
+}
+
+var requireAccessToken = function (req, res, next) {
+  if (req.access_token) {
+    res.json(resource)
+  } else {
+    res.status(401).end()
   }
 }
 
@@ -37,10 +65,11 @@ app.options("/resource", cors())
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function (req, res) {
+app.post("/resource", cors(), getAccessToken, function (req, res) {
   /*
    * Check to see if the access token was found or not
    */
+  requireAccessToken(req, res)
 })
 
 var server = app.listen(9002, "localhost", function () {
